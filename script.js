be458f28996ce01c6298b299d969b1264c4820a3
miyukihachi88-ai <miyuki.hachi.88@gmail.com>
2026-08-16 20:25:33 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -291,8 +291,8 @@ const mapMaxLng = 139.53958571726383;
 function drawMiniMap(){
     ctx.imageSmoothingEnabled = true;
     ctx.imageSmoothingQuality = "high";
-    mapCanvas.width = 300;
-    mapCanvas.height = 461;
+    mapCanvas.width = 320;
+    mapCanvas.height = 492;
     ctx.clearRect(0,0,mapCanvas.width,mapCanvas.height);
     //----------------------------------
     // 地図画像
